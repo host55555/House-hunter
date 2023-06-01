22e278d43be3250f4eee97a58113e3f04c36d7f3
karani-ken <academice79@gmail.com>
2023-06-01 16:11:02 +0300

    created the redux states for fetching agents

--- admin-system/src/features/agents/agentService.js
+++ admin-system/src/features/agents/agentService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_URL = 'http://localhost:4000/api/agents/register'
-const API_URL2=''
+const API_URL2='http://localhost:4000/api/admin/all-agents'
 //register agents by admin
 const registerAgent = async(userData, token)=>{
     const config ={
@@ -21,9 +21,12 @@ const registeredAgents = async(token)=>{
             Authorization:`Bearer ${token}`
         }
     }
+    const response = await axios.get(API_URL2,config)
+    return response.data
 }
 const agentService = {
-    registerAgent
+    registerAgent,
+    registeredAgents
 }
 
 export default agentService
--- admin-system/src/features/agents/agentSlice.js
+++ admin-system/src/features/agents/agentSlice.js
@@ -26,6 +26,14 @@ export const registerAgent = createAsyncThunk('agents/register', async(userData,
         
     }
 })
+export const registeredAgents = createAsyncThunk('agents/get-agents',async(_,thunkAPI)=>{
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await agentService.registeredAgents(token)
+    } catch (error) {
+        
+    }
+})
 
 export const agentsSlice = createSlice({
     name:'agents',
@@ -45,6 +53,20 @@ export const agentsSlice = createSlice({
         })
         .addCase(registerAgent.rejected,(state,action)=>{
             state.isLoading= false
+            state.isError=true
+            state.message=action.payload
+        })
+        .addCase(registeredAgents.pending,(state)=>{
+            state.isLoading=true
+
+        })
+        .addCase(registeredAgents.fulfilled,(state,action)=>{
+            state.isLoading=false
+            state.isSuccess=true
+            state.agents=action.payload
+        })
+        .addCase(registeredAgents.rejected,(state,action)=>{
+            state.isLoading=false
             state.isError=true
             state.message=action.payload
         })
